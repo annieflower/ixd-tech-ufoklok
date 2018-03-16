7f32abd680b66c4fd0bf25f8eb2499244afe3fc6
annieflower <>
2018-03-16 17:28:49 +0100

    commentaar toegevoegd

--- script/dagennacht.js
+++ script/dagennacht.js
@@ -2,7 +2,7 @@
 function startTime() {
     var today = new Date();
     var h = today.getHours();
-    var m = today.getMinutes();
+    var m = today.getMinutes();    
     var s = today.getSeconds();
     
     m = checkTime(m);
@@ -10,27 +10,32 @@ function startTime() {
     
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
 
-
-    if (h > 6 && h < 13) {
+// hieronder word de ochtend bepaald en ervoor gezorgd dat de afbeelding van de ochtend goed staat en de tekst
+    if (h > 6 && h < 13) {    
         document.getElementById("morning").innerHTML = "GOODMORNING";
         document.getElementsByTagName('body')[0].style.backgroundImage = "url('style/images/morning.jpg')";
-        
+
+// hieronder zorg ik ervoor dat de tekst knippert van oranje naar wit
         var tl1 = new TimelineMax()
         tl1.to('#morning', 0.5, { top: 0, color: 'orange' })
         .to('#morning', 0.5, { top: 0, color: 'white' });
     }
+// hieronder word de middag bepaald en ervoor gezorgd dat de afbeelding van de middag goed staat en de tekst
     else if (h > 12 && h < 19) {
         document.getElementById("afternoon").innerHTML = "GOOD AFTERNOON!";
         document.getElementsByTagName('body')[0].style.backgroundImage = "url('style/images/afternoon.jpg')";
 
+// hieronder zorg ik ervoor dat de tekst knippert van blauw naar wit
         var tl2 = new TimelineMax()
         tl2.to('#afternoon', 0.5, { top: 0, color: '#14addf' })
         .to('#afternoon', 0.5, { top: 0, color: 'white' });
     }
+// hieronder word de avond/nacht bepaald en ervoor gezorgd dat de afbeelding van de avond/nacht goed staat en de tekst
     else if (h > 18 && h < 7){
         document.getElementById("night").innerHTML = "GOODNIGHT!";
         document.getElementsByTagName('body')[0].style.backgroundImage = "url('style/images/night.jpg')";
 
+// hieronder zorg ik ervoor dat de tekst knippert van geel naar wit
         var tl3 = new TimelineMax()
         tl3.to('#night', 0.5, { top: 0, color: 'yellow' })
         .to('#night', 0.5, { top: 0, color: 'white' });
@@ -47,9 +52,11 @@ function checkTime(i) {
     return i;
 }
 
+// Deze var zorgt ervoor dat mijn ufo door het scherm vliegt :) 
 var tl4 = new TimelineMax({repeat: -1})
 tl4.to('#ufo', 15, { left: 1400, ease: Power1.easeOut });
 
+// Hieronder staat de functie beschreven waarin de datum op de pagina tevoorschijn komt.
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
